20171c20da04648b0f0db6bca6aed670365a98f6
David Auvray <dauvray@simplon.co>
2020-08-16 21:23:11 +0200

    WIP

---
+++ components/widgets/Feed.vue
@@ -0,0 +1,54 @@
+<template>
+        <div id="feed">
+            <div class="card">
+                <div class="card-body">
+                    <modal-widget target="changecover" class="d-flex justify-content-end"
+                                  btnclass="btn btn-light btn-lg btn-block"
+                                  @saveModalChanges="onSaveModalChanges">
+                        <template #button>
+                            <i class="fas fa-edit"></i> Commencer un post
+                        </template>
+                        <template #header>
+                            Créer un post
+                        </template>
+                        <template #body>
+
+                            <div class="author">
+                                <gravatar-widget :user="user" size="small"></gravatar-widget>
+                                {{user.name}}
+                            </div>
+
+                            <post-form></post-form>
+
+                        </template>
+                    </modal-widget>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    name: "Feed",
+    components: {
+        ModalWidget: () => import('vuejs-estarter/components/widgets/Modal'),
+        GravatarWidget: () => import('vuejs-estarter/components/widgets/Gravatar'),
+        PostForm: () => import('vuejs-socializer/components/widgets/post/PostForm')
+    },
+    props: {
+        user: {
+            type: Object,
+            required: true
+        },
+    },
+    methods: {
+        onSaveModalChanges() {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/widgets/post/PostForm.vue
@@ -0,0 +1,59 @@
+<template>
+    <form @submit.prevent>
+
+        <div class="form-group">
+            <select id="target" class="form-control form-control-sm" v-model="selected">
+                <option disabled value="">Qui peut voir votre post ?</option>
+                <option value="1">Tous le monde</option>
+                <option value="2">Relations uniquement</option>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <textarea class="form-control"
+                      v-model="postContent"
+                      placeholder="De quoi souhaitez-vous discuter..." rows="3"
+            ></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary"
+                :disabled="postContent.length >= 1 ? false : true"
+                @click="onPublishPost">Publier</button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: "PostForm",
+    data() {
+        return {
+            postContent: '',
+            selected: ''
+        }
+    },
+    methods: {
+        onPublishPost() {
+            let formData = new FormData()
+            formData.append('target', this.selected )
+            formData.append('content', this.postContent )
+
+            // is in vuejs-estarter framework ?
+            if(typeof this.$estarterSettings === 'undefined') {
+                axios.post('/publish-post', formData)
+                    .then((response) => {
+
+                    })
+                    .catch((error) => {
+                        alert(error);
+                    });
+            } else {
+                this.$emit('onPublishPost', formData)
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
